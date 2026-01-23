2a4d2c2b1f8295652ab989f5d1c2ef851dc6d83e
Jeet Chauhan <spgtjeet123@gmail.com>
2026-01-23 17:06:03 +0530

    Implementing Pattern and Bodmas in Program file

--- C#/ConsoleApp1/ConsoleApp1/Program.cs
+++ C#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,4 +8,44 @@ static void Calc()
     Calculator obj1 = new();
     obj1.Menu();
 }
-Calc();
+//Calc();
+
+static void Pattern()
+{
+    Patterns obj2 = new();
+    //central pyramid
+    //obj2.Pt1();
+
+    //central pyramid with even odd place
+    //obj2.Pt2();
+
+    //left side pyramid
+    obj2.Pt3();
+
+    //right side pyramid
+    obj2.Pt4();
+
+    //inverted left pyramid
+    obj2.Pt5();
+
+    //inverted right pyramid
+    obj2.Pt6();
+
+    //inverted central pyramid
+    obj2.Pt7();
+
+    //inverted central even odd pyramid
+    obj2.Pt8();
+
+    //numeric 1-10 left pyramid
+    obj2.Pt9();
+}
+//Pattern();
+
+static void Expression()
+{
+    Bodmas obj3 = new();
+    obj3.Accept();
+}
+//Expression();
+
